c24c9363b49f5cde07062e776e0abcd9fb5122c5
dot-rob <137016952+dot-rob@users.noreply.github.com>
2023-07-02 10:08:16 -0400

    bspwm.sh

---
+++ bspwm.sh
@@ -0,0 +1,10 @@
+sudo apt install xorg xinit bspwm sxhkd polybar suckless-tools alacritty geany pcmanfm feh hsetroot compton xfce4-power-manager system-config-printer network-manager-gnome linux-headers-$(uname -r) build-essential net-tools nmap htop cups firefox-esr remmina fonts-clear-sans fonts-liberation2 zip unzip
+
+echo "exec bspwm" >> ~/.xinitrc
+mkdir .config 
+cd ~/.config/
+mkdir bspwm sxhkd polybar
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+chmod +x ~/.config/bspwm/bspwmrc
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+cp /etc/polybar/config.ini ~/.config/polybar/
