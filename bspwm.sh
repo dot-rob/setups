0f3049e9bd6e87b8f7b203481c2cc9df17e6764d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2023-07-05 21:06:15 -0400

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,8 +1,1 @@
-sudo apt install xorg xinit bspwm sxhkd polybar suckless-tools alacritty geany pcmanfm feh hsetroot compton brightnessctl lxappearance arc-theme papirus-icon-theme system-config-printer network-manager-gnome xfce4-power-manager xfce4-polkit
-
-mkdir ~/.config && mkdir ~/.config/bspwm ~/.config/sxhkd ~/.config/alacritty
-
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-chmod +x ~/.config/bspwm/bspwmrc
-echo "exec bspwm" >> ~/.xinitrc
+sudo apt install xorg xinit bspwm sxhkd polybar suckless-tools alacritty geany pcmanfm feh hsetroot compton brightnessctl lxappearance arc-theme papirus-icon-theme system-config-printer network-manager-gnome xfce4-power-manager
