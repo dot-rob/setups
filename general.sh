82074c6cdc327cf573b2a8b5a55708e49aa3c208
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-02-11 10:27:55 -0500

    Update general.sh

--- general.sh
+++ general.sh
@@ -1,1 +1,1 @@
-linux-headers-$(uname -r) openfortivpn remmina putty chromium build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip git
+linux-headers-$(uname -r) openfortivpn remmina putty chromium build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip
