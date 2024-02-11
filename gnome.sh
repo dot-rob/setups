5b9bb552d2780b4ce0ed3aca06079efefda978f0
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-02-11 11:05:09 -0500

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,1 +1,1 @@
-sudo apt install gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-software gnome-system-monitor gnome-text-editor gnome-tweaks evince nautilus tilix
+sudo apt install gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-software gnome-system-monitor gnome-text-editor gnome-tweaks evince geany nautilus tilix
