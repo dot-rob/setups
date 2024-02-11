7d7d7505fbfed6a5e0b4d6be6ee90e274996ffb4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-02-11 10:27:23 -0500

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,1 +1,1 @@
-sudo apt install kde-plasma-desktop kde-spectacle plasma-nm pkexec ark okular gwenview kcalc kate kwrite vlc linux-headers-$(uname -r) openfortivpn remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups
+sudo apt install kde-plasma-desktop plasma-nm ark okular gwenview kde-spectacle kcalc kate kwrite vlc pkexec 
