cc9279cf22faa053a800a13e08f0c134c8e4901a
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-02-11 10:20:55 -0500

    Delete bspwm.sh

--- bspwm.sh
+++
@@ -1,1 +0,0 @@
-sudo apt install xorg xinit bspwm sxhkd polybar suckless-tools alacritty geany pcmanfm feh hsetroot compton brightnessctl system-config-printer network-manager-gnome xfce4-power-manager linux-headers-$(uname -r) build-essential net-tools nmap htop cups firefox-esr remmina fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 zip unzip
