448b0e08fc9a3144fafc541b9e44dfda302c7918
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-03-10 10:36:19 -0400

    Create xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,1 @@
+sudo apt install linux-headers-$(uname -r) openfortivpn remmina putty chromium build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip
