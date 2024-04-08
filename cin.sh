13a7a29a9be2060d3517e98127ad4ad7fff240bf
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-04-07 21:33:12 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,1 +1,1 @@
-sudo apt install cinnamon gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor tilix nero
+sudo apt install cinnamon gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor tilix nemo
