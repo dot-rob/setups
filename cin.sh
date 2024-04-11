fc231befb1c554c666e4e8b3d58754d760c56941
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-04-11 09:39:07 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,1 +1,1 @@
-sudo apt install cinnamon gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor evince tilix nemo
+sudo apt install cinnamon gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor evince celluloid shotwell tilix nemo
