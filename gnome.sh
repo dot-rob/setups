4570c8a00c3f7cd5d9b085c6a96e342d505cae1c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-04-11 15:01:31 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,1 +1,1 @@
-sudo apt install gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-software gnome-system-monitor gnome-text-editor gnome-tweaks evince celluloid nautilus shotwell tilix fonts-cantarell
+sudo apt install gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-software gnome-system-monitor gnome-tweaks evince celluloid nautilus shotwell tilix fonts-cantarell
