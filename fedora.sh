6df521fe22c86ce5f9ed177b27eb31e02859b9ab
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-05-13 09:42:39 -0400

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,1 +1,1 @@
-sudo dnf install gnome-screenshot gnome-themes-extra gnome-tweaks remmina putty nmap htop intel-clear-sans-fonts jetbrains-mono-fonts liberation-fonts openfortivpn
+sudo dnf install -y gnome-screenshot gnome-themes-extra gnome-tweaks remmina putty nmap htop intel-clear-sans-fonts jetbrains-mono-fonts liberation-fonts openfortivpn
