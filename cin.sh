7258c5f389458fa967c7668128f49e71cfae995e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-05-13 09:40:40 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,1 +1,2 @@
-sudo apt install cinnamon gnome-calculator gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor tilix nemo shotwell celluloid evince
+sudo apt install -y cinnamon gnome-calculator gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor tilix nemo shotwell celluloid evince
+sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 openfortivpn cups zip unzip
