9e23d567aab3f8bc08d5410539dab8295e3cd9cc
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-06-12 17:33:23 -0400

    Delete cin.sh

--- cin.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y cinnamon gnome-calculator gnome-disk-utility gnome-screenshot gnome-software gnome-system-monitor tilix nemo shotwell celluloid evince
-sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 openfortivpn cups zip unzip
