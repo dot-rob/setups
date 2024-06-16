7f8d2b5c10aa7f9e9442fdf17c44af52444abae4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-06-16 17:10:55 -0400

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -1,7 +1,7 @@
 sudo apt install -y sway swaybg swayidle swayimg swaylock light xwayland qt5-style-plugins waybar wofi pipewire-audio network-manager-gnome system-config-printer
-sudo apt install -y firefox-esr remmina putty cups zip unzip
+sudo apt install -y firefox-esr remmina putty fonts-clear-sans cups zip unzip
 mkdir -p ~/.config/sway ~/.config/waybar ~/.config/wofi ~/.config/foot
 cp /etc/sway/config ~/.config/sway/config
-cp /etc/xdg/waybar/config ~/.config/waybar
-cp /etc/xdg/foot/foot.ini ~/.config/foot/
+cp /etc/xdg/waybar/config ~/.config/waybar/config
+cp /etc/xdg/foot/foot.ini ~/.config/foot/foot.ini
 chmod +x ~/.config/sway/*.sh
