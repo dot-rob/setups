8490771840a4689b369bdb78586ca69406987609
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-06-16 16:56:23 -0400

    Create sway.sh

---
+++ sway.sh
@@ -0,0 +1,2 @@
+sudo apt install -y sway swaybg swayidle swayimg swaylock light xwayland qt5-style-plugins waybar wofi pipewire-audio network-manager-gnome system-config-printer
+sudo apt install -y firefox-esr remmina putty cups zip unzip
