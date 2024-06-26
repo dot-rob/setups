1d777d5d20185b969da8d166e5abf9e1e2782abc
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-06-25 21:59:03 -0400

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,2 @@
+sudo dnf install -y gnome-screenshot gnome-tweaks tilix remmina putty nmap htop openfortivpn fonts liberation-fonts mozilla-fira-sans-fonts mozilla-fira-mono-fonts
+sudo dnf remove -y gnome-contacts gnome-calendar
