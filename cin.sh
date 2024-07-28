caae76bb5a79abc0199103d66fda2bdebe762d5d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-07-28 17:25:26 -0400

    Create cin.sh

---
+++ cin.sh
@@ -0,0 +1,2 @@
+sudo apt install -y xfce4 lightdm lightdm-gtk-greeter-settings cinnamon tilix parole galculator atril menulibre geany pipewire-audio
+sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 openfortivpn cups zip unzip
