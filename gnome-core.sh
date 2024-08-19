626d30e324aa559225c257bb08bc1bef907fd27f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-08-19 17:44:54 -0400

    Delete gnome-core.sh

--- gnome-core.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y gnome-core gnome-clocks gnome-screenshot gnome-tweaks tilix celluloid
-sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 openfortivpn cups zip unzip
