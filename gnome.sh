695af75a90cd5dcf522853b68395edb776842b66
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-10 19:39:31 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,4 +1,5 @@
-sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid
 sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
