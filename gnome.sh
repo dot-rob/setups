c7af68c4b367c2fc42d0b128e64f3d0b85125544
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-10 09:52:08 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,2 +1,9 @@
 sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid
 sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups zip unzip
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
+
+
+echo "Debian script finished! Please reboot now."
+
