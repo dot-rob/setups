5bdb735de3411dd812e6a7ba706355b48f0ab740
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-09 20:57:50 -0400

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -2,5 +2,8 @@ sudo dnf install -y gnome-screenshot gnome-extensions-app gnome-shell-extension-
 sudo dnf remove -y gnome-contacts gnome-calendar simple-scan
 sudo dnf update && sudo dnf upgrade -y
 sudo dnf install -y @virtualization
-usermod -aG libvirt rob
-usermod -aG dialout rob
+sudo usermod -aG libvirt rob
+sudo usermod -aG dialout rob
+echo "Fedora script complete! Please reboot now."
+
+
