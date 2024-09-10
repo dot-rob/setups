7b10f85bb9de76312a7ebec2d25fffea05618d51
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-09 21:10:16 -0400

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,4 +1,4 @@
-sudo dnf update && sudo dnf upgrade -y
+sudo dnf update -y && sudo dnf upgrade -y
 sudo dnf install -y gnome-screenshot gnome-extensions-app gnome-shell-extension-user-theme gnome-tweaks tilix tilix-nautilus remmina putty nmap htop liberation-fonts mozilla-fira-sans-fonts mozilla-fira-mono-fonts
 sudo dnf remove -y gnome-contacts gnome-calendar simple-scan
 sudo dnf install -y @virtualization
