4175573406295f39e22d06b605982500a9224352
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-11 19:26:00 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,9 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y gnome-screenshot gnome-extensions-app gnome-shell-extension-user-theme gnome-tweaks gnome-themes-extra tilix tilix-nautilus remmina putty nmap htop liberation-fonts mozilla-fira-sans-fonts mozilla-fira-mono-fonts
 sudo apt remove -y gnome-contacts gnome-calendar simple-scan
-sudo apt install -y @virtualization
-sudo usermod -aG libvirt rob
-sudo usermod -aG dialout rob
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
 echo ""
 echo ""
 echo "Fedora script complete! Please reboot now."
