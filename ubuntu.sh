096545cb6ea75ed28794142bd1859ba546574a85
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-11 19:39:08 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,11 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y gnome-screenshot gnome-extensions-app gnome-shell-extension-user-theme gnome-tweaks gnome-themes-extra tilix tilix-nautilus remmina putty nmap htop liberation-fonts mozilla-fira-sans-fonts mozilla-fira-mono-fonts
-sudo apt remove -y gnome-contacts gnome-calendar simple-scan
+sudo apt install -y gnome-core gnome-clocks gnome-screenshot gnome-tweaks tilix celluloid firefox
+sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
 echo ""
-echo "Fedora script complete! Please reboot now."
+echo "Debian script finished! Please reboot now."
+echo ""
+echo ""
