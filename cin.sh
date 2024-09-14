e99f1788988bf3bf21747f7d7e88430c8d2653ed
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-14 17:29:38 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,6 +1,6 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y cinnamon-core gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-text-editor tilix eog evince celluloid firefox-esr
-sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip
+sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
