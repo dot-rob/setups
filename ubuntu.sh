ad592c10305167050c868e3d457934493f81936c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-14 17:03:51 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,6 +3,8 @@ sudo apt install -y gnome-core gnome-clocks gnome-screenshot gnome-tweaks tilix 
 
 vanilla-gnome-desktop vanilla-gnome-default-settings
 
+system-config-printer-common system-config-printer-udev
+
 sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
