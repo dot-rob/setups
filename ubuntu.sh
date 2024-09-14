7708d2e663fbf4658a3a5e5f8a7040aac33438be
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-14 17:32:07 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,11 @@
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y gnome-core gnome-clocks gnome-screenshot gnome-tweaks tilix celluloid firefox
-
-vanilla-gnome-desktop vanilla-gnome-default-settings
-
-system-config-printer-common system-config-printer-udev
-
-sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
+ssudo apt update -y && sudo apt upgrade -y
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid firefox 
+sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
 echo ""
-echo "Debian script finished! Please reboot now."
+echo "Ubuntu script finished! Please reboot now."
 echo ""
 echo ""
