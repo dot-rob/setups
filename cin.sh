6ff389760ab3c1183eb77a347841060551d5ae96
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-13 20:57:29 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,5 +1,5 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y cinnamon-core gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-text-editor gnome-themes-extra tilix eog evince celluloid
+sudo apt install -y cinnamon-core gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-text-editor tilix eog evince celluloid
 sudo apt install -y linux-headers-$(uname -r) remmina putty firefox-esr build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
