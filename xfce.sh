73ad2ed70a4fe47b0befdac3faf3a06ce0b0001f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-13 21:11:20 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,2 +1,10 @@
 sudo apt install -y xfce4 lightdm lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-notifyd xfce4-power-manager xfce4-screenshooter xfce4-taskmanager thunar-archive-plugin xarchiver tilix ristretto parole galculator atril menulibre geany gigolo gvfs gvfs-backends mousepad network-manager-gnome system-config-printer firefox-esr
 sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups zip unzip
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Debian script finished! Please reboot now."
+echo ""
+echo ""
