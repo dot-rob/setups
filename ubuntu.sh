c3c245c77068769d6efec73586ba69d92aa3b14f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-14 17:28:58 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ vanilla-gnome-desktop vanilla-gnome-default-settings
 
 system-config-printer-common system-config-printer-udev
 
-sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups zip unzip
+sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
