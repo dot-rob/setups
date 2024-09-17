04820c043e2b4f0338e71aa7e7a3f49f323d21ec
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-17 07:44:11 -0400

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,2 +1,10 @@
 sudo apt install -y kde-plasma-desktop plasma-nm ark okular gwenview kde-spectacle kcalc kate kwrite dragonplayer print-manager pkexec firefox-esr
 sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 openprinting-ppds cups zip unzip
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "KDE script finished! Please reboot now."
+echo ""
+echo ""
