006a2f102e62c0a7d0b6a2e24cf54ddacf0a303e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-22 13:01:20 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,11 +1,11 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y xfce4 lightdm lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-notifyd xfce4-power-manager xfce4-screenshooter xfce4-taskmanager thunar-archive-plugin xarchiver tilix ristretto parole galculator atril menulibre geany gigolo gvfs gvfs-backends mousepad network-manager-gnome system-config-printer firefox-esr
-sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 cups openprinting-ppds zip unzip
+sudo apt install -y linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-clear-sans fonts-jetbrains-mono cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
 echo ""
-echo "Debian script finished! Please reboot now."
+echo "Debian XFCE script finished! Please reboot now."
 echo ""
 echo ""
