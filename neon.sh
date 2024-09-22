739f7e308c3cd11308cff84b727558403fa344d7
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-22 12:57:26 -0400

    Delete neon.sh

--- neon.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install -y dragonplayer build-essential remmina putty nmap htop fonts-jetbrains-mono fonts-liberation2 openprinting-ppds
-sudo apt install -y virt-manager
-sudo adduser rob libvirt
-sudo adduser rob dialout
-echo ""
-echo ""
-echo "Neon script finished! Please reboot now."
-echo ""
-echo ""
