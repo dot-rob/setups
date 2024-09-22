7542bd1016d8a4a36a879add68dee5a58799ec8e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-22 13:00:00 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,11 +1,11 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid firefox-esr 
-sudo apt install -y linux-headers-$(uname -r) remmina putty build-essential iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks tilix tilix-nautilus nautilus eog evince celluloid firefox-esr 
+sudo apt install -y linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
 echo ""
-echo "Debian script finished! Please reboot now."
+echo "Debian Gnome script finished! Please reboot now."
 echo ""
 echo ""
