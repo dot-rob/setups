397917055c947042aab5aa662601fe513db6418d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-09-30 15:56:45 -0400

    Create mint.sh

---
+++ mint.sh
@@ -0,0 +1,10 @@
+sudo apt remove -y drawing gnome-calendar guarchmap hypnotix pix rhythmbox simple-scan thingy thunderbird* transmission-gtk warpinator webbapp-manager
+sudo apt install -y remmina putty nmap htop
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Linux Mint script finished! Please reboot now."
+echo ""
+echo ""
