306d7121fdc9ca60f9efbcd6eda508cfc33a94fb
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-02 16:27:09 -0400

    Create cin.sh

---
+++ cin.sh
@@ -0,0 +1,10 @@
+sudo apt install -y cinnamon gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-text-editor nemo tilix file-roller eog evince celluloid firefox-esr 
+sudo apt install -y linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 cups openprinting-ppds zip unzip
+sudo apt install -y virt-manager
+sudo adduser rob libvirt
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Debian Cinnamon script finished! Please reboot now."
+echo ""
+echo ""
