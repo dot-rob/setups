bb1561deee39bcf745cdcac96c00bf5f1b962dda
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-16 14:16:02 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,4 +1,4 @@
-sudo apt remove -y drawing gnome-calendar gucharmap hypnotix pix pix-data rhythmbox rhythmbox-data simple-scan thingy thunderbird thunderbird-locale* transmission-gtk warpinator webapp-manager
+sudo apt remove -y drawing gnome-calendar gucharmap hypnotix onboard pix pix-data *rhythmbox* seahorse simple-scan sticky thingy thunderbird thunderbird-locale* transmission-gtk warpinator webapp-manager
 sudo apt install -y remmina putty nmap htop fonts-jetbrains-mono
 sudo apt install -y virt-manager
 sudo adduser rob dialout
