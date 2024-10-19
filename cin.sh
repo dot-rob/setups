7078860a81db1c189dcb34856b5a8c60abd3419d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-19 10:48:05 -0400

    Update cin.sh

--- cin.sh
+++ cin.sh
@@ -1,5 +1,5 @@
-sudo apt install -y cinnamon gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks tilix nautilus eog evince celluloid
-sudo apt install -y linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds zip unzip
+sudo apt install -y cinnamon gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-text-editor tilix eog evince celluloid
+sudo apt install -y linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-jetbrains.mono cups openprinting-ppds zip unzip
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
@@ -13,6 +13,6 @@ sudo apt update -y
 sudo apt install firefox -y
 echo ""
 echo ""
-echo "Debian Gnome script finished! Please reboot now."
+echo "Debian Cinnamon script finished! Please reboot now."
 echo ""
 echo ""
