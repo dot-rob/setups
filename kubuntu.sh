3762bed8260a67782e49f243481718e5b0a7a4d7
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-19 10:44:50 -0400

    Delete kubuntu.sh

--- kubuntu.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install -y okular vlc remmina putty nmap htop fonts-jetbrains-mono openfortivpn openprinting-ppds
-sudo apt install -y virt-manager
-sudo adduser rob dialout
-echo ""
-echo ""
-echo "Kubuntu script finished! Please reboot now."
-echo ""
-echo ""
