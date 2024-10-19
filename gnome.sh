31e4e6d7326e95f2cac011e629eed4a38a65f08b
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-19 10:44:05 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -5,6 +5,14 @@ sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
 echo ""
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo ' Package: * Pin: origin packages.mozilla.org Pin-Priority: 1000 ' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update -y
+sudo apt install firefox -y
+echo ""
+echo ""
 echo "Debian Gnome script finished! Please reboot now."
 echo ""
 echo ""
