5adeebebadc9eab3e950fb75001c1530b0e215e8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-24 19:46:17 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,5 +1,5 @@
 sudo apt remove -y baobab dc gnome-calendar gucharmap hypnotix mintchat onboard pix pix-data pix-dbg seahorse simple-scan sticky thingy thunderbird* transmission-gtk webapp-manager
-sudo apt install -y remmina putty nmap htop fonts-jetbrains-mono
+sudo apt install -y remmina putty spotify-client nmap htop fonts-jetbrains-mono
 sudo apt install -y virt-manager
 sudo adduser rob dialout
 echo ""
