3fb92b52392cc1577f1139a2571cf7b84856f1d6
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-24 19:39:03 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,4 +1,4 @@
-sudo apt remove -y baobab dc gnome-calendar gnome-font-viewer gucharmap hypnotix onboard pix-* redshift-gtk seahorse simple-scan sticky thingy thunderbird thunderbird-locale* transmission-gtk webapp-manager
+sudo apt remove -y baobab dc gnome-calendar gucharmap hypnotix onboard pix-* redshift-gtk seahorse simple-scan sticky thingy thunderbird thunderbird-locale* transmission-gtk webapp-manager
 sudo apt install -y remmina putty nmap htop fonts-jetbrains-mono
 sudo apt install -y virt-manager
 sudo adduser rob dialout
