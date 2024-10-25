c77b515f802123b692552ccb9270051278ea7905
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-25 17:49:09 -0400

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,6 +1,6 @@
 sudo dnf install -y 
 sudo dnf remove gnome-calendar gnome-contacts simple-scan 
-sudo dnf install gnome-extensions-app gnome-themes-extra gnome-tweaks mozilla-fira* tilix remmina putty iproute2 nmap htop
+sudo dnf install gnome-extensions-app gnome-themes-extra gnome-tweaks mozilla-fira* tilix remmina putty nmap htop
 sudo dnf install -y @virtualization
 sudo usermod -aG libvirt rob
 sudo usermod -aG dialout rob
