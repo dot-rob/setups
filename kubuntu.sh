7e1afea4aadf5775826a8ed15253fd6bfce6f7f3
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-29 15:23:45 -0400

    Update kubuntu.sh

--- kubuntu.sh
+++ kubuntu.sh
@@ -1,17 +1,9 @@
-sudo apt install -y okular gwenview kcalc kate vlc 
-sudo apt install -y remmina putty nmap htop fonts-jetbrains-mono openfortivpn cups
+sudo apt install -y okular gwenview
+sudo apt install -y remmina putty firefox nmap htop openfortivpn vlc fonts-jetbrains-mono cups
 sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 sudo adduser rob lpadmin
-echo ""
-echo ""
-sudo install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-echo ' Package: * Pin: origin packages.mozilla.org Pin-Priority: 1000 ' | sudo tee /etc/apt/preferences.d/mozilla
-sudo apt update -y
-sudo apt install firefox -y
 echo ""
 echo ""
 echo "Kubuntu script finished! Please reboot now."
