0de64d75b78c0a1088d1dc3eb32fc10381b22213
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-10-29 18:40:29 -0400

    Update kubuntu.sh

--- kubuntu.sh
+++ kubuntu.sh
@@ -1,9 +1,6 @@
-sudo apt install -y okular gwenview
-sudo apt install -y remmina putty firefox nmap htop openfortivpn vlc fonts-jetbrains-mono cups
+sudo apt install -y okular remmina putty firefox nmap htop vlc cups fonts-jetbrains-mono openfortivpn 
 sudo apt install -y virt-manager
-sudo adduser rob libvirt
 sudo adduser rob dialout
-sudo adduser rob lpadmin
 echo ""
 echo ""
 echo "Kubuntu script finished! Please reboot now."
