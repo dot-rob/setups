ecbfeedc08753f250d2624d1274856f6a8829da8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-11-27 10:02:58 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,8 +1,5 @@
 sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
 sudo dnf install -y gnome-extensions-app gnome-themes-extra gnome-tweaks remmina putty mozilla-fira* htop nmap celluloid tilix tilix-nautilus openfortivpn
-sudo dnf install -y @virtualization
-sudo usermod -aG libvirt rob
-sudo usermod -aG dialout rob
 echo ""
 echo ""
 echo "Fedora script finished! Please reboot now."
