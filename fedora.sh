470d8b52730091b9b886bf8e0118daaebf1461e8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-12-04 08:03:24 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,7 +1,5 @@
 sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
-sudo dnf install -y gnome-extensions-app gnome-themes-extra gnome-tweaks remmina putty mozilla-fira* htop nmap celluloid tilix tilix-nautilus openfortivpn
-echo ""
-echo ""
+sudo dnf install -y gnome-tweaks gnome-themes-extra remmina putty mozilla-fira* htop nmap celluloid tilix tilix-nautilus openfortivpn
 sudo usermod -aG dialout rob
 echo ""
 echo ""
