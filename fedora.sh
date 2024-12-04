fbec080dae521a824e42df439ad777a59d411a9c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-12-04 08:05:12 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,6 +1,5 @@
 sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
 sudo dnf install -y gnome-tweaks gnome-themes-extra remmina putty mozilla-fira* htop nmap celluloid tilix tilix-nautilus openfortivpn
-sudo usermod -aG dialout rob
 echo ""
 echo ""
 echo "Fedora script finished! Please reboot now."
