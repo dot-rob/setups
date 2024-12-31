5421f6b69ee225db2474f281434d0bbd233e95d2
dot-rob <137016952+dot-rob@users.noreply.github.com>
2024-12-31 18:37:28 -0500

    Delete fedora.sh

--- fedora.sh
+++
@@ -1,7 +0,0 @@
-sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
-sudo dnf install -y gnome-tweaks gnome-themes-extra remmina putty mozilla-fira* htop nmap celluloid tilix tilix-nautilus openfortivpn
-echo ""
-echo ""
-echo "Fedora script finished! Please reboot now."
-echo ""
-echo ""
