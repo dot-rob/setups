e4cbe7c917c71b42e60b5a7603ff4cf2c74cb58f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-01 11:54:07 -0500

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,6 +1,9 @@
 sudo apt install -y gnome-shell baobab gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-sushi gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather low-memory-monitor pipewire-audio nautilus tilix eog evince celluloid build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip libreoffice-calc libreoffice-writer libreoffice-gnome
 echo ""
 echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
 sudo install -d -m 0755 /etc/apt/keyrings
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
