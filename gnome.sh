b4e9fd8a8984e4c4f4fef898e06d037ef43c9143
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-01 11:51:25 -0500

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gnome-shell baobab gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-sushi gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather low-memory-monitor pipewire-audio nautilus tilix eog evince celluloid build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
+sudo apt install -y gnome-shell baobab gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-sushi gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather low-memory-monitor pipewire-audio nautilus tilix eog evince celluloid build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip libreoffice-calc libreoffice-writer libreoffice-gnome
 echo ""
 echo ""
 sudo install -d -m 0755 /etc/apt/keyrings
