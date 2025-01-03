f2ebf1e56be848f26af630cc9f59ae3f0bb8a847
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-03 12:14:47 -0500

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks low-memory-monitor pipewire-audio nautilus tilix eog evince celluloid build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-text-editor gnome-themes-extra gnome-tweaks pipewire-audio low-memory-monitor build-essential nautilus tilix celluloid eog evince remmina putty iproute2 nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
 echo ""
 echo ""
 sudo adduser rob dialout
