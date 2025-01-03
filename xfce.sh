2c0236e825b5976dc37d489e55cd17e65f952140
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-03 12:23:56 -0500

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,7 +1,7 @@
 sudo apt install -y xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal pipewire-audio file-roller thunar-archive-plugin ristretto parole atril geany mousepad gigolo gvfs gvfs-backends network-manager-gnome system-config-printer linux-headers-$(uname -r) build-essential remmina putty iproute2 nmap htop fonts-liberation2 fonts-clear-sans fonts-jetbrains-mono cups openprinting-ppds zip unzip
 echo ""
 echo ""
-sudo apt install virt-manager
+sudo apt install -y virt-manager
 sudo adduser rob libvirt
 sudo adduser rob dialout
 echo ""
