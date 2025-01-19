dacec42b8a1810eb30a1729f8abaa44a3a516d2e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-19 12:55:17 -0500

    Create sid.sh

---
+++ sid.sh
@@ -0,0 +1,12 @@
+sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks pipewire-audio low-memory-monitor build-essential nautilus tilix celluloid eog evince remmina putty nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 10
+echo ""
+echo ""
+echo "Debian Gnome script finished! Please reboot now."
+echo ""
+echo ""
