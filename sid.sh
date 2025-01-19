84da6b088c4dfd21cffe326131517c27ea22bac9
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-19 13:01:20 -0500

    Update sid.sh

--- sid.sh
+++ sid.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks pipewire-audio low-memory-monitor build-essential nautilus tilix celluloid eog evince remmina putty nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
+sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell-extensions gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks firefox-esr pipewire-audio low-memory-monitor build-essential nautilus tilix celluloid eog evince remmina putty nmap htop fonts-liberation2 fonts-cantarell cups openprinting-ppds openfortivpn zip unzip
 echo ""
 echo ""
 sudo adduser rob dialout
