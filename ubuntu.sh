154604526b153e17fc5c4cad7b1b14ce54476dbf
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-28 12:28:52 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-shell-extensions gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather build-essential file-roller nautilus tilix celluloid nano eog evince remmina putty nmap htop fonts-cantarell fonts-ubuntu fonts-ubuntu-console fonts-liberation fonts-liberation2 cups iputils-ping iputils-tracepath openprinting-ppds openfortivpn
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-shell-extensions gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather build-essential nautilus tilix celluloid evince file-roller loupe firefox remmina putty nmap htop iputils-ping iputils-tracepath nano fonts-cantarell fonts-liberation fonts-liberation2 fonts-ubuntu fonts-ubuntu-console cups openprinting-ppds openfortivpn
 echo ""
 echo ""
 sudo adduser rob dialout
