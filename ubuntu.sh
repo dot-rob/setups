1603c38ef08d13e10b05c0c7ca3524fb90b24b84
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-28 12:15:08 -0500

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,12 @@
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-shell-extensions gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather build-essential file-roller nautilus tilix celluloid nano eog evince remmina putty nmap htop fonts-cantarell fonts-ubuntu fonts-ubuntu-console fonts-liberation fonts-liberation2 cups iputils-ping iputils-tracepath openprinting-ppds openfortivpn
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 10
+echo ""
+echo ""
+echo "Ubuntu Gnome script finished! Please reboot now."
+echo ""
+echo ""
