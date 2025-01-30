daa8dd3a9b9962838764a81b84fc943de947c21c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-30 17:21:27 -0500

    Update ubumin.sh

--- ubumin.sh
+++ ubumin.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gnome-shell-extensions gnome-snapshot gnome-tweaks gnome-weather build-essential tilix celluloid evince file-roller loupe remmina putty nmap htop nano fonts-clear-sans fonts-jetbrains-mono openfortivpn
+sudo apt install -y gnome-shell-extensions gnome-snapshot gnome-tweaks gnome-weather build-essential celluloid evince file-roller loupe remmina putty nmap htop nano fonts-clear-sans fonts-jetbrains-mono openfortivpn
 echo ""
 echo ""
 sudo adduser rob dialout
