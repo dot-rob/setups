2f7488da83beed533e30349c5dd769a3b90b9951
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-30 16:45:21 -0500

    Create ubumin.sh

---
+++ ubumin.sh
@@ -0,0 +1,12 @@
+sudo apt install -y gnome-shell-extensions gnome-power-manager gnome-session-canberra gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather update-manager build-essential nautilus tilix celluloid evince file-roller loupe firefox remmina putty nmap htop iputils-ping iputils-tracepath alsa-base alsa-utils bash-completion nano fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation fonts-liberation2 fonts-ubuntu fonts-ubuntu-console openprinting-ppds openfortivpn yaru-theme-gtk yaru-theme-icon yaru-theme-sound
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 10
+echo ""
+echo ""
+echo "Ubuntu script finished! Please reboot now."
+echo ""
+echo ""
