2b217341a21168af0d246f12af3d27550e3d5d24
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-01-30 13:20:41 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-shell-extensions gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather build-essential nautilus tilix celluloid evince file-roller loupe firefox remmina putty nmap htop iputils-ping iputils-tracepath nano fonts-cantarell fonts-liberation fonts-liberation2 fonts-ubuntu fonts-ubuntu-console openprinting-ppds openfortivpn
+sudo apt install -y gnome-shell gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-shell-extensions gnome-power-manager gnome-session-canberra gnome-snapshot gnome-software gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather update-manager build-essential nautilus tilix celluloid evince file-roller loupe firefox remmina putty nmap htop iputils-ping iputils-tracepath alsa-base alsa-utils bash-completion nano fonts-cantarell fonts-liberation fonts-liberation2 fonts-ubuntu fonts-ubuntu-console openprinting-ppds openfortivpn yaru-theme-gtk yaru-theme-icon yaru-theme-sound
 echo ""
 echo ""
 sudo adduser rob dialout
