cd82620a84d1cf54edb6b069bf821eb3c4439bd7
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-18 20:20:23 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather bash-completion build-essential celluloid eog evince fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation fonts-noto htop nautilus nmap openfortivpn cups openprinting-ppds pipewire-audio putty remmina tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather bash-completion build-essential celluloid eog evince fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn cups openprinting-ppds pipewire-audio putty remmina tlp tlp-rdw
 echo ""
 echo ""
 sudo adduser rob dialout
