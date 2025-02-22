5ab7168c8e8975fe9e67dadd35274d709b48ed77
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 10:33:20 -0500

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,14 @@
+flatpak install -y flathub org.angryip.ipscan
+flatpak install -y flathub com.bitwarden.desktop
+flatpak install -y flathub com.raggesilver.BlackBox
+flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.gnome.Loupe
+flatpak install -y flathub org.gnome.Maps
+flatpak install -y flathub org.gnome.Papers
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub org.wireshark.Wireshark
+flatpak install -y flathub app.zen_browser.zen
