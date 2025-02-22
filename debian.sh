4d3e3aa1cf8163fffbdf1a0768e788ef2c464c00
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 12:37:18 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,18 +1,11 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks bash-completion build-essential celluloid eog evince fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn tilix cups openprinting-ppds pipewire-audio putty remmina tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-tweaks bash-completion build-essential fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn tilix cups openprinting-ppds pipewire-audio tlp tlp-rdw
+sudo apt install -y flatpak gnome-software-plugin-flatpak
 echo ""
 echo ""
 sudo adduser rob dialout
 echo ""
 echo ""
 sleep 3
-echo ""
-echo ""
-sudo install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-echo ' Package: * Pin: origin packages.mozilla.org Pin-Priority: 1000 ' | sudo tee /etc/apt/preferences.d/mozilla
-sudo apt update -y
-sudo apt install firefox -y
 echo ""
 echo ""
 echo "Debian script finished! Please reboot now."
