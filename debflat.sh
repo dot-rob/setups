67d06ee3ede59bd65c5eafb02d6e1a8d03987e29
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 11:57:26 -0500

    Update debflat.sh

--- debflat.sh
+++ debflat.sh
@@ -1,7 +1,5 @@
 sudo apt install -y linux-headers-$(uname -r) gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-tweaks bash-completion build-essential fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn tilix cups openprinting-ppds pipewire-audio tlp tlp-rdw
-echo ""
-echo ""
-sudo apt install flatpak -y gnome-software-plugin-flatpak
+sudo apt install -y flatpak gnome-software-plugin-flatpak
 echo ""
 echo ""
 sudo adduser rob dialout
