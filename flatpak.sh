ceb92cd6871e15579565d1c86a4724e51f59e66c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 16:37:51 -0500

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,13 +1,6 @@
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y flathub io.github.celluloid_player.Celluloid
-flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gnome.Calculator
-flatpak install -y flathub org.gnome.clocks
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Papers
-flatpak install -y flathub org.gnome.TextEditor
-flatpak install -y flathub org.gnome.Weather
 flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.remmina.Remmina
-flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
