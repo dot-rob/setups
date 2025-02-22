6fa137f5ae288391e361b88e8c71c3847dae0c28
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 09:56:37 -0500

    Create debflat.sh

---
+++ debflat.sh
@@ -0,0 +1,12 @@
+sudo apt install -y linux-headers-$(uname -r) gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather bash-completion build-essential fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn cups openprinting-ppds pipewire-audio tlp tlp-rdw
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo ""
+echo ""
+echo "Debian script finished! Please reboot now."
+echo ""
+echo ""
