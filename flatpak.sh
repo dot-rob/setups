0149c73650e615aa78fdc263e76cf810706cbdc8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 11:15:52 -0500

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,18 +1,19 @@
 sudo apt install flatpak -y gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.angryip.ipscan
-flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub com.rafaelmardojai.Blanket
+
+flatpak install flathub io.missioncenter.MissionCenter
 flatpak install flathub org.gnome.Calculator
 flatpak install flathub org.gnome.clocks
-flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install flathub org.gnome.Weather
+flatpak install -y flathub app.zen_browser.zen
+flatpak install -y flathub com.bitwarden.desktop
 flatpak install -y flathub com.google.Chrome
-flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Maps
 flatpak install -y flathub org.gnome.Papers
-flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
-flatpak install flathub io.missioncenter.MissionCenter
-flatpak install flathub org.gnome.Weather
-flatpak install -y flathub app.zen_browser.zen
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
