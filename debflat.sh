c5cf7d05e45e153750933e334049eb44b76cddf4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 11:00:00 -0500

    Update debflat.sh

--- debflat.sh
+++ debflat.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-calculator gnome-clocks gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather bash-completion build-essential fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn cups openprinting-ppds pipewire-audio tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-tweaks gnome-weather bash-completion build-essential fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop nautilus nmap openfortivpn cups openprinting-ppds pipewire-audio tlp tlp-rdw
 echo ""
 echo ""
 sudo adduser rob dialout
