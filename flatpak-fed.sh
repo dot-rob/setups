9b249982ba22374a93fa9f74e48217378f53f0f8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 21:32:10 -0500

    Create flatpak-fed.sh

---
+++ flatpak-fed.sh
@@ -0,0 +1,10 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub io.gitlab.news_flash.NewsFlash
+flatpak install -y flathub org.angryip.ipscan
+flatpak install -y flathub org.gnome.Loupe
+flatpak install -y flathub org.gnome.Papers
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
