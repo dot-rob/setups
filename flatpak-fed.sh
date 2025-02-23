b7075a9449519426f149ba1296b26b8cf25022df
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 22:07:44 -0500

    Update flatpak-fed.sh

--- flatpak-fed.sh
+++ flatpak-fed.sh
@@ -4,7 +4,6 @@ flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub io.gitlab.news_flash.NewsFlash
 flatpak install -y flathub org.angryip.ipscan
-flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Papers
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
