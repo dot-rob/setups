d16f322117fe7f20f4285b23a6d3e96a6356bd97
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-22 21:28:49 -0500

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,3 +1,20 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub io.gitlab.news_flash.NewsFlash
+flatpak install -y flathub org.angryip.ipscan
+flatpak install -y flathub org.gnome.Calculator
+flatpak install -y flathub org.gnome.clocks
+flatpak install -y flathub org.gnome.Loupe
+flatpak install -y flathub org.gnome.Maps
+flatpak install -y flathub org.gnome.Papers
+flatpak install -y flathub org.gnome.Weather
+flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+
+
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub org.angryip.ipscan
