d9883d693c43f1ea2b01f2b2f24a8514381cb440
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-24 07:43:26 -0500

    Create flatpak-kde.sh

---
+++ flatpak-kde.sh
@@ -0,0 +1,21 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.kde.akregator
+flatpak install flathub org.kde.ark
+flatpak install flathub org.kde.dolphin
+flatpak install flathub org.kde.gwenview
+flatpak install flathub org.kde.isoimagewriter
+flatpak install flathub org.kde.kalk
+flatpak install flathub org.kde.kate
+#flatpak install flathub org.kde.kcalc
+flatpak install flathub org.kde.kclock
+flatpak install flathub org.kde.kfind
+flatpak install flathub org.kde.konsole
+flatpak install flathub org.kde.kweather
+flatpak install flathub org.kde.kwrite
+flatpak install flathub org.kde.okular
+flatpak install flathub org.videolan.VLC
+flatpak install -y flathub com.rafaelmardojai.Blanket
+#flatpak install -y flathub org.angryip.ipscan
+flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
