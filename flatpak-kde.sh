ad617c7951729202c5471338083efe7c702e673c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-24 08:56:42 -0500

    Update flatpak-kde.sh

--- flatpak-kde.sh
+++ flatpak-kde.sh
@@ -1,19 +1,19 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.kde.akregator
-flatpak install flathub org.kde.ark
-flatpak install flathub org.kde.dolphin
-flatpak install flathub org.kde.gwenview
-flatpak install flathub org.kde.isoimagewriter
-flatpak install flathub org.kde.kalk
-flatpak install flathub org.kde.kate
-#flatpak install flathub org.kde.kcalc
-flatpak install flathub org.kde.kclock
-flatpak install flathub org.kde.kfind
-flatpak install flathub org.kde.konsole
-flatpak install flathub org.kde.kweather
-flatpak install flathub org.kde.kwrite
-flatpak install flathub org.kde.okular
-flatpak install flathub org.videolan.VLC
+flatpak install -y flathub org.kde.akregator
+flatpak install -y flathub org.kde.ark
+flatpak install -y flathub org.kde.dolphin
+flatpak install -y flathub org.kde.gwenview
+flatpak install -y flathub org.kde.isoimagewriter
+flatpak install -y flathub org.kde.kalk
+flatpak install -y flathub org.kde.kate
+#flatpak install -y flathub org.kde.kcalc
+flatpak install -y flathub org.kde.kclock
+flatpak install -y flathub org.kde.kfind
+flatpak install -y flathub org.kde.konsole
+flatpak install -y flathub org.kde.kweather
+flatpak install -y flathub org.kde.kwrite
+flatpak install -y flathub org.kde.okular
+flatpak install -y flathub org.videolan.VLC
 flatpak install -y flathub com.rafaelmardojai.Blanket
 #flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.mozilla.firefox
