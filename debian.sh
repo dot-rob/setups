53b533ec6f144740bf85bad345f698d92309f2fe
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-24 16:25:43 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,5 +1,6 @@
 sudo apt install -y linux-headers-$(uname -r) gnome-disk-utility gnome-keyring gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-tweaks bash-completion build-essential cups fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation foomatic-db htop nautilus nmap openprinting-ppds pipewire-audio tilix tlp tlp-rdw
 sudo apt install -y flatpak gnome-software-plugin-flatpak
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
 echo ""
 echo ""
 sudo adduser rob dialout
