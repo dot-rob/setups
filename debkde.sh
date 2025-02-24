f34cad54345f36231f5a514f5dadd52624f7bbcf
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-24 07:41:01 -0500

    Create debkde.sh

---
+++ debkde.sh
@@ -0,0 +1,11 @@
+sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm bash-completion build-essential cups fonts-clear-sans fonts-jetbrains-mono fonts-liberation fonts-noto foomatic-db htop nmap openprinting-ppds pipewire-audio tlp tlp-rdw
+sudo apt install -y flatpak gnome-software-plugin-flatpak
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian KDE script finished! Please reboot now."
+echo ""
+echo ""
