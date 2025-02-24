1222e57c676dd07dbcf1ad3cd66df53b353f8f42
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-02-24 09:50:17 -0500

    Update flatpak-kde.sh

--- flatpak-kde.sh
+++ flatpak-kde.sh
@@ -1,7 +1,7 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.kde.akregator
 flatpak install -y flathub org.kde.ark
-flatpak install -y flathub org.kde.dolphin
+#flatpak install -y flathub org.kde.dolphin
 flatpak install -y flathub org.kde.gwenview
 flatpak install -y flathub org.kde.isoimagewriter
 flatpak install -y flathub org.kde.kalk
@@ -9,7 +9,7 @@ flatpak install -y flathub org.kde.kate
 #flatpak install -y flathub org.kde.kcalc
 flatpak install -y flathub org.kde.kclock
 flatpak install -y flathub org.kde.kfind
-flatpak install -y flathub org.kde.konsole
+#flatpak install -y flathub org.kde.konsole
 flatpak install -y flathub org.kde.kweather
 flatpak install -y flathub org.kde.kwrite
 flatpak install -y flathub org.kde.okular
