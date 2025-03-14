9c7a38ee640c4e0965a58ec40b1c0ff22aa74d03
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-14 09:02:55 -0400

    Update debkde.sh

--- debkde.sh
+++ debkde.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm bash-completion build-essential cups foomatic-db dolphin fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop konsole nmap pipewire-audio tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm bash-completion build-essential cups foomatic-db print-manager dolphin fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop konsole nmap pipewire-audio tlp tlp-rdw
 sudo apt install -y flatpak plasma-discover-backend-flatpak
 echo ""
 echo ""
