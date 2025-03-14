5b65c64e75044d70569c0baf3998cfeeddcdffd9
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-14 18:11:20 -0400

    Update debkde-flatpak.sh

--- debkde-flatpak.sh
+++ debkde-flatpak.sh
@@ -1,7 +1,7 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-flatpak install -y flathub org.kde.akregator
+#flatpak install -y flathub org.kde.akregator
 flatpak install -y flathub org.kde.ark
 flatpak install -y flathub org.kde.gwenview
 flatpak install -y flathub org.kde.isoimagewriter
