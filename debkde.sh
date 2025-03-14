4da67e06f1a84192b8b41a96bcd12b63cebb6ca0
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-14 07:32:23 -0400

    Update debkde.sh

--- debkde.sh
+++ debkde.sh
@@ -1,6 +1,5 @@
 sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm bash-completion build-essential cups foomatic-db dolphin fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop konsole nmap pipewire-audio tlp tlp-rdw
 sudo apt install -y flatpak plasma-discover-backend-flatpak
-sudo flatpak override --filesystem=xdg-config/gtk-3.0
 echo ""
 echo ""
 sudo adduser rob dialout
