dca79770cf041c7fee40cc0885a5d0764889e9a4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-14 07:24:34 -0400

    Create debkde.sh

---
+++ debkde.sh
@@ -0,0 +1,12 @@
+sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm bash-completion build-essential cups foomatic-db dolphin fonts-clear-sans fonts-jetbrains-mono fonts-liberation htop konsole nmap pipewire-audio tlp tlp-rdw
+sudo apt install -y flatpak plasma-discover-backend-flatpak
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian KDE script finished! Please reboot now."
+echo ""
+echo ""
