83793a97ad395080f2f8b1645cf50cf4d2bed83d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-15 18:16:09 -0400

    Update debkde-flatpak.sh

--- debkde-flatpak.sh
+++ debkde-flatpak.sh
@@ -14,7 +14,7 @@ flatpak install -y flathub org.kde.kwrite
 flatpak install -y flathub org.kde.okular
 flatpak install -y flathub org.videolan.VLC
 #flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y flathub org.angryip.ipscan
+#flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
