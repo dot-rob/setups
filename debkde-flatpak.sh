25a2902bba478acddbc4fda19f81a860f7bc67c9
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-16 10:04:12 -0400

    Update debkde-flatpak.sh

--- debkde-flatpak.sh
+++ debkde-flatpak.sh
@@ -1,6 +1,7 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
+flatpak install -y flathub com.google.Chrome
 flatpak install -y flathub org.kde.gwenview
 flatpak install -y flathub org.kde.isoimagewriter
 flatpak install -y flathub org.kde.kalk
@@ -11,6 +12,6 @@ flatpak install -y flathub org.kde.kclock
 flatpak install -y flathub org.kde.kwrite
 flatpak install -y flathub org.kde.okular
 flatpak install -y flathub org.videolan.VLC
-flatpak install -y flathub org.mozilla.firefox
+#flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
