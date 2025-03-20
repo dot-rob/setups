21b9e6bb07352ed097baa8462c053355d306d222
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-20 08:11:11 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra gnome-tweaks bash-completion build-essential cups fonts-cantarell fonts-clear-sans fonts-jetbrains-mono fonts-liberation foomatic-db htop nautilus nmap pipewire-audio tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra gnome-tweaks bash-completion build-essential cups fonts-cantarell fonts-clear-sans fonts-inter fonts-jetbrains-mono fonts-liberation foomatic-db htop nautilus nmap pipewire-audio tlp tlp-rdw
 sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
 echo ""
 echo ""
