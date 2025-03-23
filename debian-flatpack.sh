22d38620c2eea42b9ecd9080e7eb06e64f2f70f1
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-22 21:24:01 -0400

    Update debian-flatpack.sh

--- debian-flatpack.sh
+++ debian-flatpack.sh
@@ -1,12 +1,11 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-#flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub com.bitwarden.desktop
+#flatpak install -y flathub com.google.Chrome
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub io.github.celluloid_player.Celluloid
-#flatpak install -y flathub io.github.realmazharhussain.GdmSettings
 flatpak install -y flathub io.gitlab.news_flash.NewsFlash
 flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gnome.Calculator
@@ -17,6 +16,6 @@ flatpak install -y flathub org.gnome.Papers
 flatpak install -y flathub org.gnome.TextEditor
 flatpak install -y flathub org.gnome.Weather
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
-#flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
