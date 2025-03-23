8ba753ccee3d13f0c5bf3f127a441da0f964cc1e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-23 12:20:21 -0400

    Update debian-flatpack.sh

--- debian-flatpack.sh
+++ debian-flatpack.sh
@@ -9,4 +9,5 @@ flatpak install -y flathub io.gitlab.news_flash.NewsFlash
 flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Papers
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.mozilla.firefox
