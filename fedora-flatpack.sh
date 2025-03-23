040e625a43e9d30a86dc957a1f0131dcf2edebfc
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-23 10:11:21 -0400

    Update fedora-flatpack.sh

--- fedora-flatpack.sh
+++ fedora-flatpack.sh
@@ -1,8 +1,8 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-#flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub com.bitwarden.desktop
+#flatpak install -y flathub com.google.Chrome
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub io.github.celluloid_player.Celluloid
