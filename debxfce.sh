ee8a410af102fbf549b069f9bd4b6cd02ed4f075
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-27 18:50:35 -0400

    Update debxfce.sh

--- debxfce.sh
+++ debxfce.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal libpolkit-agent-1-0 pipewire-audio file-roller thunar-archive-plugin ristretto parole atril geany mousepad gigolo gvfs gvfs-backends network-manager-gnome system-config-printer build-essential remmina putty iproute2 nmap htop fonts-clear-sans fonts-jetbrains-mono cups openprinting-ppds zip unzip
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal libpolkit-agent-1-0 pipewire-audio file-roller thunar-archive-plugin ristretto parole atril geany mousepad gigolo gvfs gvfs-backends network-manager-gnome system-config-printer build-essential remmina putty nmap htop fonts-clear-sans fonts-jetbrains-mono cups openprinting-ppds zip unzip
 sudo apt install -y flatpak
 echo ""
 echo ""
