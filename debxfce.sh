b168ca2286a110b514e975738c3cf6e63eef97c1
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-27 19:42:28 -0400

    Update debxfce.sh

--- debxfce.sh
+++ debxfce.sh
@@ -1,13 +1,10 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal libpolkit-agent-1-0 pipewire-audio file-roller thunar-archive-plugin ristretto parole atril geany mousepad gigolo gvfs gvfs-backends network-manager-gnome system-config-printer build-essential remmina putty nmap htop fonts-clear-sans fonts-jetbrains-mono cups openprinting-ppds zip unzip tlp tlp-rdw
-sudo apt install -y flatpak
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential colloid cups file-roller fonts-clear-sans fonts-jetbrains-mono geany gigolo gvfs gvfs-backends htop libpolkit-agent-1-0 mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio putty remmina ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
 echo ""
 echo ""
 sudo adduser rob dialout
 echo ""
 echo ""
 sleep 3
-echo ""
-echo ""
 echo "Debian XFCE script finished! Please reboot now."
 echo ""
 echo ""
