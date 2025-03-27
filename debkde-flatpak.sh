4d608c57b2b31e6848f82223aa8f01d4f7d21519
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-27 18:38:29 -0400

    Update debkde-flatpak.sh

--- debkde-flatpak.sh
+++ debkde-flatpak.sh
@@ -1,9 +1,6 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub com.spotify.Client
-flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.kde.isoimagewriter
 flatpak install -y flathub org.kde.kalk
