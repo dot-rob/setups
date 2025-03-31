28e2189f151cd45b7f7c1cefa30b5085c3a7f28c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-30 22:33:15 -0400

    Delete neon-flatpak.sh

--- neon-flatpak.sh
+++
@@ -1,5 +0,0 @@
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-flatpak install -y flathub org.kde.isoimagewriter
-flatpak install -y flathub org.kde.kalk
-flatpak install -y flathub org.kde.kclock
