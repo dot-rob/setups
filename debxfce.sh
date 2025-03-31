5cb933fe726881fec792bc77856f8502fc6f443d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-03-30 21:11:13 -0400

    Update debxfce.sh

--- debxfce.sh
+++ debxfce.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential colloid cups fonts-clear-sans fonts-jetbrains-mono geany gigolo gvfs gvfs-backends htop libpolkit-agent-1-0 mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio putty remmina ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential cups fonts-clear-sans fonts-jetbrains-mono geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio putty remmina ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
 echo ""
 echo ""
 sudo adduser rob dialout
