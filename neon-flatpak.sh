32335b542893c3f5d46238da4bdee58d657855e4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-04 22:42:27 -0400

    Create neon-flatpak.sh

---
+++ neon-flatpak.sh
@@ -0,0 +1,5 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub org.kde.isoimagewriter
+flatpak install -y flathub org.kde.kalk
+flatpak install -y flathub org.kde.kclock
