c490da7fb9f4598ada009668dd895245140f6409
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-07 17:06:55 -0400

    Create debxfce.sh

---
+++ debxfce.sh
@@ -0,0 +1,11 @@
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential cups fonts-clear-sans fonts-jetbrains-mono geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio putty remmina ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
+sudo apt install -y flatpak
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian XFCE script finished! Please reboot now."
+echo ""
+echo ""
