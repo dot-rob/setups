ca528ffc55f26420816a5a6c69628d7cb1026895
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-07 17:15:27 -0400

    Update debxfce.sh

--- debxfce.sh
+++ debxfce.sh
@@ -5,6 +5,14 @@ echo ""
 sudo adduser rob dialout
 echo ""
 echo ""
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo ' Package: * Pin: origin packages.mozilla.org Pin-Priority: 1000 ' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update -y
+sudo apt install firefox -y
+echo ""
+echo ""
 sleep 3
 echo "Debian XFCE script finished! Please reboot now."
 echo ""
