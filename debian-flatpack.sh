23394a52e874e7f4a76a9b8a733cdd448c4d42d1
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-08 10:06:20 -0400

    Update debian-flatpack.sh

--- debian-flatpack.sh
+++ debian-flatpack.sh
@@ -2,7 +2,6 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
 flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.gnome.Calculator
 flatpak install -y flathub org.gnome.Loupe
