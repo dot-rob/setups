6f038c13ef71040cd36b2802eff9b8e7d26138ac
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-08 08:08:24 -0400

    Create debxfce.sh

---
+++ debxfce.sh
@@ -0,0 +1,18 @@
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential cups fonts-clear-sans fonts-jetbrains-mono geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio putty remmina ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo ' Package: * Pin: origin packages.mozilla.org Pin-Priority: 1000 ' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update -y
+sudo apt install firefox -y
+echo ""
+echo ""
+sleep 3
+echo "Debian XFCE script finished! Please reboot now."
+echo ""
+echo ""
