b2f43e14803eaca6c394f3ce46f13bf61addc80e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-12 14:23:33 -0400

    Update debxfce-flat.sh

--- debxfce-flat.sh
+++ debxfce-flat.sh
@@ -4,3 +4,11 @@ sudo flatpak override --filesystem=xdg-config/gtk-3.0
 flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.mozilla.firefox
+
+flatpak install -y flathub org.remmina.Remmina
+
+flatpak install -y flathub org.xfce.mousepad
+flatpak install -y flathub org.xfce.ristretto
+
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+
