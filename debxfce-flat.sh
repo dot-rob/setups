efc9766b6ca4db10b91dfba692286363ca3e5219
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-12 14:33:23 -0400

    Update debxfce-flat.sh

--- debxfce-flat.sh
+++ debxfce-flat.sh
@@ -6,7 +6,6 @@ flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub org.videolan.VLC
 flatpak install -y flathub org.xfce.mousepad
