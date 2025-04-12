85075873f4d89c4fc106306fcb833b58f228d4e4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-12 14:06:43 -0400

    Create debxfce-flat.sh

---
+++ debxfce-flat.sh
@@ -0,0 +1,6 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
+
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y flathub org.mozilla.firefox
