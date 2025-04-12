71b05af89a5c3ea38556bbd90b2b293cf2604dcb
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-12 10:33:02 -0400

    Update debkde-flat.sh

--- debkde-flat.sh
+++ debkde-flat.sh
@@ -1,5 +1,6 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.kde.isoimagewriter
 flatpak install -y flathub org.kde.kcalc
 flatpak install -y flathub org.kde.kclock
