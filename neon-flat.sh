88e21ebd8e9eda5b97227ee920d703687aa8f4bf
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-12 13:13:31 -0400

    Create neon-flat.sh

---
+++ neon-flat.sh
@@ -0,0 +1,10 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
+
+flatpak install -y flathub com.bitwarden.desktop
+flatpak install -y flathub org.angryip.ipscan
+flatpak install -y flathub org.kde.isoimagewriter
+flatpak install -y flathub org.kde.kclock
+flatpak install -y flathub org.kde.kweather
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
