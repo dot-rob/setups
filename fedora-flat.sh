d2dec6918c808a5735581b071d51e6d85ab30236
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-13 16:00:28 -0400

    Update fedora-flat.sh

--- fedora-flat.sh
+++ fedora-flat.sh
@@ -5,7 +5,6 @@ flatpak install -y flathub com.bitwarden.desktop
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.angryip.ipscan
-flatpak install -y flathub org.gnome.Papers
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
