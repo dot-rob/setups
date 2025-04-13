f5e14f5ca157e8fc712f713aaf5b70e3905b02c1
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-13 16:20:36 -0400

    Update debian-flat.sh

--- debian-flat.sh
+++ debian-flat.sh
@@ -15,3 +15,4 @@ flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+flatpak install -y flathub us.zoom.Zoom
