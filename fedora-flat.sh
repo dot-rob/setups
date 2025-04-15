8cb904c45efd4f2c54f5828b875ab0505a38ddf3
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-15 14:25:12 -0400

    Update fedora-flat.sh

--- fedora-flat.sh
+++ fedora-flat.sh
@@ -1,7 +1,6 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-flatpak install -y flathub com.bitwarden.desktop
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.angryip.ipscan
