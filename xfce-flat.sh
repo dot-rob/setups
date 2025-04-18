aacbd673a5ec556dacb7952fec99b96d8e4f3639
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-18 12:50:00 -0400

    Update xfce-flat.sh

--- xfce-flat.sh
+++ xfce-flat.sh
@@ -2,5 +2,8 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
 flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
