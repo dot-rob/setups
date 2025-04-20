ea2057f2bc76911c1092554e675e296192744126
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-20 17:13:22 -0400

    Create neon.sh

---
+++ neon.sh
@@ -0,0 +1,10 @@
+sudo apt install -y build-essential fonts-jetbrains-mono htop isoimagewriter kcalc kclock kweather nmap
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "KDE Neon script finished! Please reboot now."
+echo ""
+echo ""
