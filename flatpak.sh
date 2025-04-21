fd72c6dc8a547196a517950f129114211fe5dc72
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-21 06:36:29 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,33 +1,18 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 
-#all
 flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub com.spotify.Client
-flatpak install -y flathub org.angryip.ipscan
-flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.remmina.Remmina
-flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
-
-#gnome
 flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub io.github.celluloid_player.Celluloid
-flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y flathub org.angryip.ipscan
 flatpak install -y flathub org.gnome.Calculator
 flatpak install -y flathub org.gnome.clocks
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Papers
 flatpak install -y flathub org.gnome.TextEditor
 flatpak install -y flathub org.gnome.Weather
-
-#kde
-flatpak install -y flathub org.kde.gwenview
-flatpak install -y flathub org.kde.isoimagewriter
-flatpak install -y flathub org.kde.kate
-flatpak install -y flathub org.kde.kcalc
-flatpak install -y flathub org.kde.kclock
-flatpak install -y flathub org.kde.kfind
-flatpak install -y flathub org.kde.kweather
-flatpak install -y flathub org.kde.kwrite
-flatpak install -y flathub org.kde.okular
-flatpak install -y flathub org.videolan.VLC
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
