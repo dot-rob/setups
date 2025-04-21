864c544148c23c5d28c680e92f8db7f1eda75525
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-21 17:21:00 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-terminal atril build-essential cups file-roller fonts-inter fonts-jetbrains-mono fonts-liberation geany gigolo gvfs gvfs-backends htop menulibre mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio policykit-1-gnome ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager tilix atril build-essential cups file-roller fonts-inter fonts-jetbrains-mono fonts-liberation geany gigolo gvfs gvfs-backends htop menulibre mousepad network-manager-gnome nmap openprinting-ppds parole pipewire-audio policykit-1-gnome ristretto system-config-printer thunar-archive-plugin tlp tlp-rdw unzip zip
 sudo apt install -y flatpak
 echo ""
 echo ""
