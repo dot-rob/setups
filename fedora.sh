3426e3a83925f2e90ef2f98dcbb87a41783cff64
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-21 20:45:10 -0400

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,5 +1,5 @@
 sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
-sudo dnf install -y gnome-themes-extra gnome-tweaks foomatic htop kernel-devel nmap
+sudo dnf install -y gnome-themes-extra gnome-tweaks foomatic htop kernel-devel nmap tilix
 echo ""
 echo ""
 sudo usermod -aG dialout rob
