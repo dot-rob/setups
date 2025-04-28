ec99205800d4caf3accd9bc64faefe3ab2f5b652
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-28 16:47:51 -0400

    Create mint.sh

---
+++ mint.sh
@@ -0,0 +1,11 @@
+sudo apt remove -y baobab dc gnome-calendar hypnotix mintchat onboard pix pix-data pix-dbg seahorse simple-scan sticky thingy thunderbird* transmission-gtk webapp-manager
+sudo apt install -y remmina putty spotify-client nmap htop fonts-jetbrains-mono
+sudo apt install -y flatpak
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Linux Mint script finished! Please reboot now."
+echo ""
+echo ""
