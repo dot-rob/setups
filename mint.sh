b2bce49ed0f9cc40128db245b4d0c1d66771e2b6
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-28 16:48:19 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,6 +1,5 @@
 sudo apt remove -y baobab dc gnome-calendar hypnotix mintchat onboard pix pix-data pix-dbg seahorse simple-scan sticky thingy thunderbird* transmission-gtk webapp-manager
 sudo apt install -y remmina putty spotify-client nmap htop fonts-jetbrains-mono
-sudo apt install -y flatpak
 echo ""
 echo ""
 sudo adduser rob dialout
