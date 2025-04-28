8aea73f55a0e46e576e171cb2c989260732c8aeb
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-28 17:01:46 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,4 +1,4 @@
-sudo apt remove -y baobab dc gnome-calendar hypnotix mintchat onboard pix pix-data pix-dbg seahorse simple-scan sticky thingy thunderbird* transmission-gtk webapp-manager
+sudo apt remove -y baobab dc gnome-calendar hypnotix mintchat onboard pix pix-data pix-dbg seahorse simple-scan sticky thingy transmission-gtk webapp-manager
 sudo apt install -y remmina putty spotify-client nmap htop fonts-jetbrains-mono
 echo ""
 echo ""
