580d013f0ed41e52ffc7914066f940a912f954d1
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-04-29 07:50:24 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,5 +1,5 @@
 sudo apt remove -y gnome-calendar hypnotix mintchat onboard seahorse simple-scan sticky thingy transmission-gtk warpinator webapp-manager
-sudo apt install -y htop nmap putty remmina spotify-client tilix
+sudo apt install -y geany htop nmap putty remmina spotify-client tilix
 echo ""
 echo ""
 sudo adduser rob dialout
