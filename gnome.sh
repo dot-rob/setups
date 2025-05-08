7b4e358aaa7a0e08cdb00e05c2aba799b1c6e16e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-08 13:15:45 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks bash-completion build-essential cups fonts-liberation foomatic-db htop low-memory-monitor nautilus nmap openfortivpn pipewire-audio putty remmina tilix tlp tlp-rdw
+sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks bash-completion build-essential cups fonts-liberation2 foomatic-db htop low-memory-monitor nautilus nmap openfortivpn pipewire-audio putty remmina tilix tlp tlp-rdw
 sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
 echo ""
 echo ""
