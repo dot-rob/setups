db5148ea5d4abae5ec41f3c6f5ba9aeba2784421
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-08 13:04:33 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -3,17 +3,16 @@ sudo flatpak override --filesystem=/home/rob/.themes/
 
 #all
 flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.spotify.Client
 
 #gnome
-flatpak install -y flathub com.discordapp.Discord
+
 flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 flatpak install -y flathub org.gnome.Calculator
 flatpak install -y flathub org.gnome.clocks
-flatpak install -y flathub org.geany.Geany
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.gnome.Papers
-flatpak install -y flathub org.gnome.TextEditor
 flatpak install -y flathub org.gnome.Weather
