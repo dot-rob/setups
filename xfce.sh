6bbf4d1be242237c000f5c820afc45533c6f8680
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-08 13:24:44 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-netload-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openfortivpn parole pipewire-audio putty remmina ristretto rofi system-config-printer thunar-archive-plugin tilix tlp tlp-rdw unzip zip
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-netload-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openfortivpn parole pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin tilix tlp tlp-rdw unzip zip
 sudo apt install -y flatpak
 echo ""
 echo ""
