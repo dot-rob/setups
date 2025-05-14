f52c52fc0c28185cdc018b55ec56cbf97dc9ee5c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-14 07:46:40 -0400

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt install -y gnome-tweaks bash-completion build-essential celluloid evince htop loupe nmap openfortivpn putty remmina
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Ubuntu script finished! Please reboot now."
+echo ""
+echo ""
