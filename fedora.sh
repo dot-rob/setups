1b405f450030ae56faf3ee0922097521ddbc8a5d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-15 16:47:08 -0400

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,11 @@
+sudo dnf remove -y gnome-calendar gnome-contacts simple-scan
+sudo dnf install -y gnome-themes-extra gnome-tweaks foomatic htop kernel-devel nmap openfortivpn putty remmina tilix
+echo ""
+echo ""
+sudo usermod -aG dialout rob
+echo ""
+echo ""
+sleep 3
+echo "Fedora script finished! Please reboot now."
+echo ""
+echo ""
