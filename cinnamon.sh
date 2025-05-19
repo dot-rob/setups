f895dac1dae73c53a680d98a1ed712ef6e9c816d
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-19 12:37:59 -0400

    Create cinnamon.sh

---
+++ cinnamon.sh
@@ -0,0 +1,11 @@
+sudo apt install -y linux-headers-$(uname -r) cinnamon gnome-disk-utility gnome-screenshot gnome-text-editor gnome-weather bash-completion build-essential cups fonts-liberation2 foomatic-db htop low-memory-monitor nmap openfortivpn pipewire-audio putty remmina tilix tlp tlp-rdw
+sudo apt install -y flatpak
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian script finished! Please reboot now."
+echo ""
+echo ""
