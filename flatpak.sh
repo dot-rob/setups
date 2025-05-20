361a1f0e2231f09747b5440094fdd8985057a468
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-20 18:15:46 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -5,7 +5,6 @@ flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.gnome.Calculator
-flatpak install -y flathub org.gnome.Weather
 flatpak install -y flathub org.mozilla.firefox
 
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
