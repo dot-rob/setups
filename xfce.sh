5f20e7b6f2ecda22b6133824e9c396ee2a8ac298
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-27 19:15:30 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,5 +1,6 @@
 sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-netload-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin tilix tlp tlp-rdw wireplumber unzip zip
 sudo apt install -y flatpak
+sudo apt autoremove -y
 echo ""
 echo ""
 sudo adduser rob dialout
