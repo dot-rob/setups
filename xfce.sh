9ae160d4e31072a24b784c816b0ab4abf3156292
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-30 08:45:25 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-netload-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin tilix wireplumber unzip zip
+sudo apt install -y linux-headers-$(uname -r) xfce4 lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4-battery-plugin xfce4-netload-plugin xfce4-power-manager xfce4-screenshooter xfce4-taskmanager xfce4-whiskermenu-plugin atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin tilix wireplumber unzip zip
 sudo apt install -y flatpak
 sudo apt autoremove -y
 echo ""
