7badb1cc28b6714107181028aeb37b70eef973a5
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-05-30 20:53:26 -0400

    Create mintxfce.sh

---
+++ mintxfce.sh
@@ -0,0 +1,11 @@
+sudo apt remove -y gnome-calendar hypnotix mintchat onboard seahorse simple-scan sticky thingy timeshift transmission-gtk warpinator webapp-manager
+sudo apt purge -y thunderbird
+sudo apt install -y xfce4-battery-plugin xfce4-netload-plugin geany gigolo htop nmap openfortivpn putty qalculate-gtk remmina rofi
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Linux Mint script finished! Please reboot now."
+echo ""
+echo ""
