00b46e69a9cbd43f7af522c2892f011f76b505d4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-02 17:41:04 -0400

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,1 +1,11 @@
-
+sudo apt install -y build-essential htop isoimagewriter kcalc kclock nmap openfortivpn putty remmina
+sudo apt autoremove -y
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian KDE script finished! Please reboot now."
+echo ""
+echo ""
