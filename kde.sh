138d210263acf58038fd28b80d5ea279c0c50ca7
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-04 10:13:32 -0400

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,6 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm ark bash-completion build-essential cups dolphin fonts-jetbrains-mono foomatic-db htop kde-spectacle konsole nmap pipewire-audio pkexec print-manager
-
-
+sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm ark bash-completion build-essential cups dolphin firefox-esr fonts-jetbrains-mono fonts-liberation2 foomatic-db gwenview htop kate kcalc kde-spectacle konsole low-memory-monitor nmap okular openfortivpn pipewire pipewire-pulse pipewire-audio pkexec print-manager putty remmina vlc wireplumber
 sudo apt install -y flatpak plasma-discover-backend-flatpak
 echo ""
 echo ""
