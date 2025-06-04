833e219004c46e3d0c2e448bf9cf429a0d580e92
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-03 20:07:20 -0400

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,18 @@
+sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm ark bash-completion build-essential cups dolphin fonts-jetbrains-mono foomatic-db htop kde-spectacle konsole nmap pipewire-audio pkexec print-manager
+
+
+
+
+
+
+sudo apt install -y flatpak plasma-discover-backend-flatpak
+echo ""
+echo ""
+sudo adduser rob dialout
+sudo adduser rob lpadmin
+echo ""
+echo ""
+sleep 3
+echo "Debian KDE script finished! Please reboot now."
+echo ""
+echo ""
