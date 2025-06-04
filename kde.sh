aadc89a06a398156419e3c359028422c54693ce5
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-03 20:12:27 -0400

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,10 +1,6 @@
 sudo apt install -y linux-headers-$(uname -r) plasma-desktop sddm ark bash-completion build-essential cups dolphin fonts-jetbrains-mono foomatic-db htop kde-spectacle konsole nmap pipewire-audio pkexec print-manager
 
 
-
-
-
-
 sudo apt install -y flatpak plasma-discover-backend-flatpak
 echo ""
 echo ""
