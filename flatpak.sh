046daaed12a52caa6e0fd1aa9460095ae5214dc4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-05 12:38:25 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,7 +2,6 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 #all
 flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub io.github.shiftey.Desktop
 flatpak install -y flathub org.libreoffice.LibreOffice
 flatpak install -y flathub org.mozilla.firefox
 
@@ -19,15 +18,3 @@ flatpak install -y flathub org.gnome.Weather
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
 sudo flatpak override --filesystem=/home/rob/.themes/
-
-#kde
-flatpak install -y flathub org.kde.gwenview
-flatpak install -y flathub org.kde.isoimagewriter
-flatpak install -y flathub org.kde.kate
-flatpak install -y flathub org.kde.kcalc
-flatpak install -y flathub org.kde.kclock
-flatpak install -y flathub org.kde.kfind
-flatpak install -y flathub org.kde.kweather
-flatpak install -y flathub org.kde.kwrite
-flatpak install -y flathub org.kde.okular
-flatpak install -y flathub org.videolan.VLC
