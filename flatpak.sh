fb96119de908d90579fdb3e46f2f1d388b4679e6
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-12 19:47:07 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -11,6 +11,7 @@ flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub org.gnome.Calculator
 flatpak install -y flathub org.gnome.clocks
 flatpak install -y flathub org.gnome.Loupe
+flatpak install -y flathub org.gnome.meld
 flatpak install -y flathub org.gnome.Papers
 flatpak install -y flathub org.gnome.TextEditor
 flatpak install -y flathub org.gnome.Weather
