c0793be5d2851c4dcdb0cad6967e29473106f924
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-14 16:09:17 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -5,6 +5,8 @@ flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub org.libreoffice.LibreOffice
 flatpak install -y flathub org.mozilla.firefox
 
+##########################################
+
 #gnome
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub io.github.celluloid_player.Celluloid
@@ -19,6 +21,8 @@ flatpak install -y flathub org.gnome.Weather
 flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
 sudo flatpak override --filesystem=/home/rob/.themes/
+
+##########################################
 
 #kde
 flatpak install -y flathub org.kde.gwenview
