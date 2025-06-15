7ed8c2f7fe64d60046295597513bed080d6e47e8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-14 22:09:29 -0400

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -1,4 +1,7 @@
-sudo apt install -y linux-headers-$(uname -r) bash-completion build-essential cinnamon htop nmap
+sudo apt install -y linux-headers-$(uname -r) bash-completion build-essential cinnamon cups fonts-liberation2 foomatic-db htop low-memory-monitor nmap openfortivpn pipewire pipewire-pulse pipewire-audio putty remmina wireplumber
+
+gnome-screenshot
+
 #atril cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop low-memory-monitor mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin wireplumber
 echo ""
 echo ""
