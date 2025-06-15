d03c4a29c9910f447ea63f8bc932ca25f8587402
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-15 15:35:44 -0400

    Create mkdir.sh

---
+++ mkdir.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.config/tilix/schemes
+mkdir -p ~/.config/geany/colorschemes
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/menus
+cp ~/Downloads/schemes/nord/tilix/schemes/nord.json ~/.config/tilix/schemes/
+cp ~/Downloads/schemes/nord/geany/colorschemes/nord.conf ~/.config/geany/colorschemes/
+cp ~/Downloads/schemes/nord/rofi/config.rasi ~/.config/rofi/
+cp ~/Downloads/schemes/nord/menus/xfce-applications.menu ~/.config/menus/
