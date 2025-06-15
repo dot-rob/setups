9b4c6ea34c2a909a564b3e055fd053ed75c17ee4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-06-14 22:03:28 -0400

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -1,5 +1,5 @@
-sudo apt install -y linux-headers-$(uname -r) lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon
-#atril bash-completion build-essential cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop low-memory-monitor mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin wireplumber
+sudo apt install -y linux-headers-$(uname -r) bash-completion build-essential cinnamon
+#atril cups fonts-liberation2 foomatic-db geany gigolo gvfs gvfs-backends htop low-memory-monitor mousepad network-manager-gnome nmap openfortivpn parole pipewire pipewire-pulse pipewire-audio putty qalculate-gtk remmina ristretto rofi system-config-printer thunar-archive-plugin wireplumber
 echo ""
 echo ""
 sudo apt install -y flatpak
