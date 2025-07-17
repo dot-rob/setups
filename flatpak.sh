e0833571f0778c00620e08df22132c2cbd91c2a6
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-07-16 21:29:40 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,9 +1,9 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #all
-#flatpak install -y flathub com.discordapp.Discord
-#flatpak install -y flathub org.libreoffice.LibreOffice
-#flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub org.libreoffice.LibreOffice
+flatpak install -y flathub org.mozilla.firefox
 
 ##########################################
 
@@ -17,14 +17,13 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 ##########################################
 
 #gnome
-#flatpak install -y flathub com.rafaelmardojai.Blanket
-#flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub com.rafaelmardojai.Blanket
+flatpak install -y flathub io.github.celluloid_player.Celluloid
 #flatpak install -y flathub io.gitlab.news_flash.NewsFlash
-#flatpak install -y flathub org.gnome.Calculator
-#flatpak install -y flathub org.gnome.clocks
-#flatpak install -y flathub org.gnome.Loupe
-#flatpak install -y flathub org.gnome.Papers
-#flatpak install -y flathub org.gnome.TextEditor
-#flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
-#sudo flatpak override --env=GTK_THEME=Adwaita-dark
-#sudo flatpak override <package identifier> --filesystem=/home/rob/.themes/
+flatpak install -y flathub org.gnome.Calculator
+flatpak install -y flathub org.gnome.clocks
+flatpak install -y flathub org.gnome.Loupe
+flatpak install -y flathub org.gnome.Papers
+flatpak install -y flathub org.gnome.TextEditor
+flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+sudo flatpak override --env=GTK_THEME=Adwaita-dark
