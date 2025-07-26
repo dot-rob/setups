2d2c2715fb2fbe356a2bea833be93e5bdc61de31
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-07-25 20:30:51 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
-sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-themes-extra gnome-tweaks bash-completion build-essential cups foomatic-db htop low-memory-monitor nautilus nmap openfortivpn pipewire pipewire-pulse pipewire-audio putty remmina tilix wireplumber
+sudo apt install -y linux-headers-$(uname -r) gnome-shell gnome-shell-extensions gnome-shell-extension-arc-menu gnome-shell-extension-dash-to-panel gnome-disk-utility gnome-keyring gnome-screenshot gnome-system-monitor gnome-themes-extra gnome-tweaks bash-completion build-essential cups foomatic-db htop low-memory-monitor nautilus nmap openfortivpn pipewire pipewire-pulse pipewire-audio putty remmina tilix wireplumber
 echo ""
 echo ""
 sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
