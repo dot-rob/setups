44fc69bf51ba8f46091a9ec532581f878cb0d026
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-08-01 22:32:04 -0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,7 +1,7 @@
 sudo apt remove -y gnome-calendar simple-scan
 #sudo apt remove -y gnome-calculator gnome-calendar hypnotix mintchat onboard seahorse simple-scan sticky thingy transmission-gtk warpinator webapp-manager
 sudo apt purge -y thunderbird
-sudo apt install -y geany htop nmap putty remmina
+sudo apt install -y geany htop nmap openfortivpn putty remmina
 echo ""
 echo ""
 sudo adduser rob dialout
