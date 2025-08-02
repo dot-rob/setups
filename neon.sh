efcfff045e7a596f5989707e0e417c943934f3e4
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-08-02 11:37:26 -0400

    Create neon.sh

---
+++ neon.sh
@@ -0,0 +1,13 @@
+sudo apt install -y build-essential htop isoimagewriter kcalc kclock nmap openfortivpn putty remmina
+echo ""
+echo ""
+sudo apt autoremove -y
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "KDE Neon script finished! Please reboot now."
+echo ""
+echo ""
