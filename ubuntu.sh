9061e5906e11668c717efaf2e745e19cee3d67b8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-08-11 18:47:48 -0400

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,24 @@
+sudo apt install -y build-essential \
+celluloid \
+gnome-shell-extensions \
+gnome-tweaks \
+gnome-weather \
+htop \
+libreoffice \
+loupe \
+nmap \
+openfortivpn \
+putty \
+remmina \
+echo ""
+echo ""
+sudo apt autoremove -y
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+sleep 3
+echo "Debian GNOME script finished! Please reboot now."
+echo ""
+echo ""
