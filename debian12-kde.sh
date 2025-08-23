de01dfb5ecf6975af9cbd33f1b567125de46fe0a
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-08-23 19:06:02 -0400

    Create debian12-kde.sh

---
+++ debian12-kde.sh
@@ -0,0 +1,32 @@
+sudo apt install -y plasma-desktop sddm pipewire pipewire-audio wireplumber \
+ark \
+bash-completion \
+build-essential \
+cups \
+dolphin \
+htop \
+gwenview \
+kate \
+kcalc \
+kde-spectacle \
+konsole \
+linux-headers-amd64 \
+nmap \
+okular \
+openfortivpn \
+pkexec \
+print-manager \
+vlc \
+echo ""
+echo ""
+sudo apt install -y flatpak plasma-discover-backend-flatpak
+echo ""
+echo ""
+sudo apt autoremove -y
+echo ""
+echo ""
+sudo adduser rob dialout
+sudo adduser rob lpadmin
+echo ""
+echo ""
+echo "Debian KDE script finished! Please reboot now."
