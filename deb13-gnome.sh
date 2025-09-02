a8fe7af11bfd567570e442b66985ba1f8b308e3c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-02 10:15:25 -0400

    Update deb13-gnome.sh

--- deb13-gnome.sh
+++ deb13-gnome.sh
@@ -11,27 +11,24 @@ gnome-calculator \
 gnome-disk-utility \
 gnome-keyring \
 gnome-shell-extensions \
-gnome-shell-extension-dash-to-panel \
 gnome-system-monitor \
 gnome-text-editor \
 gnome-themes-extra \
 gnome-tweaks \
-gnome-weather \
 htop \
-libreoffice \
 linux-headers-amd64 \
 loupe \
 nautilus \
 nmap \
 openfortivpn \
 papers \
+ptyxis \
 putty \
 remmina \
-tilix \
 unattended-upgrades
 echo ""
 echo ""
-#sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
+sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
 echo ""
 echo ""
 sudo apt -y autoremove --purge
