fc4000c0539aeb175e38ee807e8b768190fd1ce8
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-02 10:16:28 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -3,8 +3,8 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 #all
 flatpak install -y flathub org.libreoffice.LibreOffice
 flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.remmina.Remmina
-flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+#flatpak install -y flathub org.remmina.Remmina
+#flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
 
 ##########################################
 
