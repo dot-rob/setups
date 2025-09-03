61a59d341e22aaa22964c9942f84e3861db11d8e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-03 09:35:49 -0400

    Create mint.sh

---
+++ mint.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt remove -y gnome-calendar simple-scan sticky thingy transmission-gtk
+sudo apt purge -y thunderbird
+sudo apt install -y geany htop nmap openfortivpn putty remmina
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Linux Mint script finished! Please reboot now."
