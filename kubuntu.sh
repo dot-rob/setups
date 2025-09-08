d8a79f5d52cd26331522ff89f8c2d43f271718c6
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-08 19:27:21 -0400

    Create kubuntu.sh

---
+++ kubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt update -y
+sudo apt install -y build-essential firefox htop kcalc kclock nmap openfortivpn putty remmina
+echo ""
+echo ""
+sudo apt autoremove -y
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Kubuntu script finished! Please reboot now."
