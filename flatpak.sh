62ea97737f723f6e325fca796b764cc409ff3074
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-13 18:50:34 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,25 +2,26 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 #all
 flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.libreoffice.LibreOffice
+#flatpak install -y flathub org.libreoffice.LibreOffice
 flatpak install -y flathub org.remmina.Remmina
 flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
 
 ##########################################
 
 #gnome
-flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y flathub io.github.celluloid_player.Celluloid
-flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
-sudo flatpak override --env=GTK_THEME=Adwaita-dark
+#flatpak install -y flathub com.rafaelmardojai.Blanket
+#flatpak install -y flathub io.github.celluloid_player.Celluloid
+#flatpak install -y flathub org.gtk.Gtk3theme.Adwaita-dark
+#sudo flatpak override --env=GTK_THEME=Adwaita-dark
 
 
 ##########################################
 
 #kde
-#flatpak install -y flathub org.kde.isoimagewriter
-#flatpak install -y flathub org.kde.kcalc
-#flatpak install -y flathub org.kde.kclock
-#flatpak install -y flathub org.videolan.VLC
+flatpak install -y flathub org.kde.isoimagewriter
+flatpak install -y flathub org.kde.kate
+flatpak install -y flathub org.kde.kcalc
+flatpak install -y flathub org.kde.kclock
+flatpak install -y flathub org.videolan.VLC
 
 ##########################################
