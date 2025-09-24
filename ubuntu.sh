65fe91c7d1a1515ea9c882012019f813173ad99f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-24 19:47:19 -0400

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,19 @@
+sudo apt update -y
+sudo apt install -y \
+build-essential \
+celluloid \
+gnome-tweaks \
+htop \
+nmap \
+openfortivpn \
+putty \
+remmina
+echo ""
+echo ""
+snap install blanket
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Ubuntu script finished! Please reboot now."
