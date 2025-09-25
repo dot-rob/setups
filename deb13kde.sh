58a02e393aa142c38fc8ac16cb1f0c31e2004b1c
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-24 21:06:49 -0400

    Create deb13kde.sh

---
+++ deb13kde.sh
@@ -0,0 +1,36 @@
+sudo apt update -y
+sudo apt install -y plasma-desktop sddm pipewire pipewire-audio wireplumber \
+ark \
+bash-completion \
+build-essential \
+cups \
+dolphin \
+htop \
+gwenview \
+kate \
+kcalc \
+kclock \
+kde-spectacle \
+konsole \
+linux-headers-amd64 \
+nmap \
+okular \
+openfortivpn \
+pkexec \
+print-manager \
+putty \
+remmina \
+vlc
+echo ""
+echo ""
+sudo apt install -y flatpak plasma-discover-backend-flatpak
+echo ""
+echo ""
+sudo apt -y autoremove --purge
+echo ""
+echo ""
+sudo adduser rob dialout
+sudo adduser rob lpadmin
+echo ""
+echo ""
+echo "Debian KDE script finished! Please reboot now."
