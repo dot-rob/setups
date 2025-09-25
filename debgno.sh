274ea6be87db03cd2615eaf6f01ee6bce51650c9
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-24 20:57:46 -0400

    Create debgno.sh

---
+++ debgno.sh
@@ -0,0 +1,38 @@
+sudo apt update -y
+sudo apt install -y gnome-shell pipewire pipewire-audio wireplumber \
+bash-completion \
+build-essential \
+cups \
+eog \
+evince \
+firefox-esr \
+gnome-calculator \
+gnome-clocks \
+gnome-disk-utility \
+gnome-keyring \
+gnome-shell-extensions \
+gnome-system-monitor \
+gnome-text-editor \
+gnome-themes-extra \
+gnome-tweaks \
+htop \
+linux-headers-amd64 \
+nautilus \
+nmap \
+openfortivpn \
+putty \
+remmina \
+tilix \
+unattended-upgrades
+echo ""
+echo ""
+sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
+echo ""
+echo ""
+sudo apt -y autoremove --purge
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Debian GNOME script finished! Please reboot now."
