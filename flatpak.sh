f73e1793c156df7c7f7318c4e8f4cc23a80f0b10
dot-rob <137016952+dot-rob@users.noreply.github.com>
2025-09-27 12:20:49 -0400

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,10 +1,10 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #all
-flatpak install -y flathub org.mozilla.firefox
+#flatpak install -y flathub org.mozilla.firefox
 #flatpak install -y flathub org.libreoffice.LibreOffice
-flatpak install -y flathub org.remmina.Remmina
-flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
+#flatpak install -y flathub org.remmina.Remmina
+#flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
 
 ##########################################
 
@@ -18,10 +18,10 @@ flatpak install -y flathub uk.org.greenend.chiark.sgtatham.putty
 ##########################################
 
 #kde
-flatpak install -y flathub org.kde.isoimagewriter
-flatpak install -y flathub org.kde.kate
-flatpak install -y flathub org.kde.kcalc
-flatpak install -y flathub org.kde.kclock
-flatpak install -y flathub org.videolan.VLC
+#flatpak install -y flathub org.kde.isoimagewriter
+#flatpak install -y flathub org.kde.kate
+#flatpak install -y flathub org.kde.kcalc
+#flatpak install -y flathub org.kde.kclock
+#flatpak install -y flathub org.videolan.VLC
 
 ##########################################
