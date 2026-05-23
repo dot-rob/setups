f5ec6a86cfbab0ef0b6eef35ee19b1a617e39816
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-05-22 21:28:24 -0400

    Update deb13xfce.sh for package installation and cleanup

--- deb13xfce.sh
+++ deb13xfce.sh
@@ -1,22 +1,26 @@
-sudo apt install -y linux-headers-$(uname -r) xfce4 pipewire pipewire-pulse pipewire-audio wireplumber \
+sudo apt update -y
+sudo apt install -y xfce4 pipewire pipewire-audio wireplumber \
+echo ""
+echo ""
 atril \
 bash-completion \
 build-essential \
 cups \
+firefox-esr \
+foomatic-db \
 geany \
 gigolo \
+gnome-disk-utility \
 gvfs \
 gvfs-backends \
 lightdm-gtk-greeter-settings \
+linux-headers-amd64 \
 mousepad \
 network-manager-gnome \
-nmap \
-openfortivpn \
 putty \
 qalculate-gtk \
 remmina \
 ristretto \
-rofi \
 system-config-printer \
 thunar-archive-plugin \
 xfce4-battery-plugin \
@@ -27,13 +31,13 @@ xfce4-taskmanager \
 xfce4-terminal
 echo ""
 echo ""
-sudo apt autoremove -y
+#sudo apt install -y flatpak xdg-desktop-portal xdg-desktop-portal-xapp xdg-desktop-portal-gtk
+echo ""
+echo ""
+sudo apt -y autoremove --purge
 echo ""
 echo ""
 sudo adduser rob dialout
 echo ""
 echo ""
-sleep 3
 echo "Debian XFCE script finished! Please reboot now."
-echo ""
-echo ""
