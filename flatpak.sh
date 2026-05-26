e53c0a976f0e74a04e4d613729bb22e25d33e92b
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-05-26 11:12:55 -0400

    Delete flatpak.sh

--- flatpak.sh
+++
@@ -1,6 +0,0 @@
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-#all
-#flatpak install -y flathub org.mozilla.firefox
-#flatpak install -y flathub com.google.Chrome
-#flatpak install -y flathub org.libreoffice.LibreOffice
