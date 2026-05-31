cf604b37d996e45798c2ddcac0e2ff2467566b1e
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-05-31 10:35:10 -0400

    Reorder xfce4-terminal in deb13xfce.sh

--- deb13xfce.sh
+++ deb13xfce.sh
@@ -30,8 +30,7 @@ xfce4-netload-plugin \
 xfce4-power-manager \
 xfce4-screenshooter \
 xfce4-taskmanager \
-xfce4-terminal \
-xfce4-whiskermenu-plugin
+xfce4-terminal
 echo ""
 echo ""
 #sudo apt install -y flatpak xdg-desktop-portal xdg-desktop-portal-xapp xdg-desktop-portal-gtk
