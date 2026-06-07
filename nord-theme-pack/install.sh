0b7cc67184fd1d5b2f41a0794496620c7d24d049
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-06-06 20:16:10 -0400

    Simplify installation message for Nord themes
    Removed 'if the schemes do not appear immediately' from the message.

--- nord-theme-pack/install.sh
+++ nord-theme-pack/install.sh
@@ -13,4 +13,4 @@ cp nord.xml "$HOME/.local/share/gtksourceview-4/styles/nord.xml"
 cp nord.xml "$HOME/.local/share/gtksourceview-3.0/styles/nord.xml"
 cp nord.rasi "$HOME/.config/rofi/themes/nord.rasi"
 
-echo "Installed Nord companion themes. Restart open apps if the schemes do not appear immediately."
+echo "Installed Nord companion themes. Restart open apps."
