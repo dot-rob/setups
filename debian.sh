19f5991b02cdd751c564d1640a066be0a1a2a30f
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-07-19 12:25:02 -0400

    Update package installation and user configuration

--- debian.sh
+++ debian.sh
@@ -1,12 +1,13 @@
 sudo apt install -y xfce4 pipewire pipewire-alsa pipewire-audio pipewire-pulse pavucontrol wireplumber \
 atril \
+arc-theme greybird-gtk-theme \
 bash-completion \
 bluetooth blueman bluez-firmware libspa-0.2-bluetooth \
 build-essential \
 cups cups-pk-helper ipp-usb \
 curl wget rsync \
 firefox-esr \
-firmware-linux-nonfree fwupd \
+firmware-linux-nonfree firmware-iwlwifi firmware-realtek fwupd \
 fonts-inter \
 fonts-jetbrains-mono \
 foomatic-db \
@@ -16,7 +17,7 @@ gvfs \
 gvfs-backends \
 gvfs-fuse \
 htop ncdu tree \
-intel-microcode \
+intel-microcode amd64-microcode \
 lightdm-gtk-greeter-settings \
 linux-headers-amd64 \
 lm-sensors smartmontools \
@@ -35,16 +36,19 @@ thunar-volman \
 tilix \
 tio \
 tumbler \
+ufw \
 unzip p7zip-full \
-xdg-utils \
+xdg-utils xdg-user-dirs \
 xfce4-battery-plugin \
 xfce4-netload-plugin \
 xfce4-power-manager \
+xfce4-pulseaudio-plugin \
 xfce4-screenshooter \
 xfce4-taskmanager
 echo ""
 echo ""
 sudo usermod -aG dialout rob
+xdg-user-dirs-update
 echo ""
 echo ""
 echo "Debian XFCE installation finished! Please reboot now."
