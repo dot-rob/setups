178ee261580391a02508d62ef944f8093f80d436
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-07-19 22:52:08 -0400

    Create debkde.sh

---
+++ debkde.sh
@@ -0,0 +1,29 @@
+sudo apt install -y plasma-desktop sddm pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio wireplumber \
+ark \
+bash-completion \
+bluedevil \
+build-essential \
+cups \
+dolphin \
+firefox-esr \
+htop \
+gwenview \
+haruna \
+kate \
+kcalc \
+kclock \
+kde-spectacle \
+konsole \
+linux-headers-amd64 \
+okular \
+openfortivpn \
+print-manager \
+remmina \
+tio
+echo ""
+echo ""
+sudo adduser rob dialout
+sudo adduser rob lpadmin
+echo ""
+echo ""
+echo "Debian KDE script finished! Please reboot now."
