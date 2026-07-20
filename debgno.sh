69aa00fe1c97e3d3dd81cf504e2acb65c71ac313
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-07-19 21:05:47 -0400

    Create Debian GNOME setup script
    This script installs various packages and adds a user to the dialout group. It also prompts the user to reboot after completion.

---
+++ debgno.sh
@@ -0,0 +1,37 @@
+sudo apt install -y gnome-shell pipewire pipewire-pulse pipewire-audio wireplumber \
+bash-completion \
+blanket \
+build-essential \
+cups \
+celluloid \
+fonts-jetbrains-mono \
+firefox-esr \
+gnome-calculator \
+gnome-clocks \
+gnome-disk-utility \
+gnome-keyring \
+gnome-shell-extensions \
+gnome-system-monitor \
+gnome-text-editor \
+gnome-themes-extra \
+gnome-tweaks \
+htop \
+linux-headers-amd64 \
+loupe \
+nautilus \
+openfortivpn \
+openssh-client \
+papers \
+putty \
+remmina \
+tilix \
+tio
+echo ""
+echo ""
+#sudo apt install -y flatpak gnome-software-plugin-flatpak xdg-desktop-portal-gnome
+echo ""
+echo ""
+sudo adduser rob dialout
+echo ""
+echo ""
+echo "Debian GNOME script finished! Please reboot now."
