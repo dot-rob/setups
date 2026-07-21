0551ab21fa1e7af9b583224944815711611646f0
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-07-20 21:19:57 -0400

    Update debkde.sh

--- debkde.sh
+++ debkde.sh
@@ -5,25 +5,36 @@ bluedevil \
 build-essential \
 cups cups-pk-helper ipp-usb \
 curl wget rsync \
+dnsutils nmap traceroute whois htop ncdu tree \
 dolphin \
+fd-find \
+ffmpegthumbs \
+filelight \
 firefox-esr \
-firmware-linux-nonfree firmware-iwlwifi firmware-realtek fwupd intel-microcode amd64-microcode \
+firmware-amd-graphics \
+firmware-linux-nonfree firmware-iwlwifi firmware-realtek fwupd intel-microcode amd64-microcode firmware-misc-nonfree \
 fonts-inter \
 fonts-jetbrains-mono \
 foomatic-db \
-htop ncdu tree \
+gvfs-backends \
 gwenview \
 haruna \
 kate \
 kcalc \
 kclock \
 kde-spectacle \
+kio-extras \
 konsole \
+ksystemlog \
 linux-headers-amd64 \
 okular \
 openfortivpn \
+partitionmanager \
+plocate \
 print-manager \
+qt6-image-formats-plugins \
 remmina \
+ripgrep \
 tio \
 ufw \
 unzip p7zip-full
