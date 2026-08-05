2be6f4f63677fb96486fd295fc9f78810b117505
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-08-04 20:31:05 -0400

    Remove obsolete packages from debxfce.sh

--- debxfce.sh
+++ debxfce.sh
@@ -21,8 +21,7 @@ firmware-linux-nonfree \
 firmware-realtek \
 fonts-inter \
 fonts-jetbrains-mono \
-foomatic-db-compressed-ppds \
-openprinting-ppds \
+foomatic-db \
 geany \
 gnome-disk-utility \
 gvfs-backends \
