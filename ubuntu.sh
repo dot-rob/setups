3f4373812ba9a138bec06cb5a9eba1babf3c7f23
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-08-21 23:02:12 -0400

    Create ubuntu.sh for installing applications and groups
    This script installs essential Ubuntu applications and adds the user to required groups.

---
+++ ubuntu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Installing Ubuntu applications..."
+
+sudo apt install -y \
+blanket \
+build-essential \
+curl \
+gnome-tweaks \
+htop \
+nmap \
+openfortivpn \
+putty \
+remmina \
+showtime \
+tio \
+virt-manager \
+whois
+
+echo ""
+echo ""
+
+echo "Adding $USER to required groups..."
+for group in dialout lpadmin; do
+  if id -nG "$USER" | grep -qw "$group"; then
+    echo "$USER is already in the $group group."
+  else
+    sudo usermod -aG "$group" "$USER"
+    echo "Added $USER to the $group group."
+  fi
+done
+
+echo ""
+echo ""
+
+echo "Ubuntu installation completed. Reboot to complete the setup."
+
+echo ""
+echo ""
