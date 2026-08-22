2e2a6f54385ed6a38598ffcaf5a89ab12957b245
dot-rob <137016952+dot-rob@users.noreply.github.com>
2026-08-21 22:59:17 -0400

    Create script to install GNOME and necessary packages
    This script installs GNOME and various applications, adds the user to required groups, and prompts for a reboot.

---
+++ debgno.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Installing GNOME and applications..."
+
+sudo apt install -y \
+gnome-shell \
+gdm3 \
+bash-completion \
+bind9-dnsutils \
+blanket \
+build-essential \
+cifs-utils \
+cups \
+cups-pk-helper \
+curl \
+firefox-esr \
+firmware-iwlwifi \
+firmware-linux-nonfree \
+firmware-realtek \
+fonts-inter \
+fonts-jetbrains-mono \
+foomatic-db \
+gnome-calculator \
+gnome-clocks \
+gnome-disk-utility \
+gnome-logs \
+gnome-session \
+gnome-shell-extension-dash-to-panel \
+gnome-shell-extensions \
+gnome-text-editor \
+gnome-tweaks \
+gvfs-backends \
+htop \
+intel-microcode \
+libspa-0.2-bluetooth \
+linux-headers-amd64 \
+loupe \
+nautilus \
+network-manager-gnome \
+nfs-common \
+nmap \
+openfortivpn \
+papers \
+pipewire-audio wireplumber \
+remmina \
+showtime \
+system-config-printer \
+tilix \
+tio \
+ufw \
+unzip \
+virt-manager \
+wget \
+whois \
+zip
+
+echo ""
+echo ""
+
+echo "Adding $USER to required groups..."
+for group in dialout lpadmin; do
+  if id -nG "$USER" | grep -qw "$group"; then
+    echo "$USER is already in the $group group."
+  else
+    sudo usermod -aG "$group" "$USER"
+    echo "Added $USER to the $group group."
+  fi
+done
+
+echo ""
+echo ""
+
+echo "GNOME installation completed. Reboot to complete the setup."
+
+echo ""
+echo ""
